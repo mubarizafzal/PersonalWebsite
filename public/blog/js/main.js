c32405d817e01eaf736aa2f9f12112d15e60cb8e
Mubariz Afzal <mubarizafzal@gmail.com>
2020-08-24 10:29:33 -0400

    swithced to triangulated background

--- public/blog/js/main.js
+++ public/blog/js/main.js
@@ -1,9 +1,15 @@
+function makeSVG (tag, attrs) {
+  var el = document.createElementNS('http://www.w3.org/2000/svg', tag);
+  for (var k in attrs)
+      el.setAttribute(k, attrs[k]);
+  return el;
+}
 
 var w = $("#container").innerWidth();
 var h = $("#container").innerHeight();
 
 
-const numPoints = 50;
+const numPoints = 40;
 
 
 var points = [[0,0],[0,h],[w,0],[w,h]];
@@ -28,14 +34,16 @@ function getY(i) {
   return (points[i][1]);
 }
 
+var canvas = document.getElementById("container");
+
 $("document").ready(function () {
   for (var i = 0; i < triangles.length; i += 3) {
+    var randNum = Math.floor(Math.random() * 30 + 200);
     var p0 = triangles[i];
     var p1 = triangles[i + 1];
     var p2 = triangles[i + 2];
-    var newTriangle = $(`<polygon points='${getX(p0)},${getY(p0)} ${getX(p1)},${getY(p1)} ${getX(p2)},${getY(p2)}'></polygon>`);
-    $("#container").append(newTriangle);
+    console.log(randNum);
+    var poly = makeSVG('polygon', {points: `${getX(p0)},${getY(p0)} ${getX(p1)},${getY(p1)} ${getX(p2)},${getY(p2)}`, fill: `rgb(${randNum},${randNum},${randNum})`});
+    canvas.appendChild(poly);
   }
 });
-
-$("#container").append("<p>why</p>");
--- public/js/main.js
+++ public/js/main.js
@@ -8,3 +8,55 @@ $("document").ready(function () {
   $(".projects-card-binding").animate({left: 0}, 1000);
 });
 
+
+function makeSVG (tag, attrs) {
+  var el = document.createElementNS('http://www.w3.org/2000/svg', tag);
+  for (var k in attrs)
+      el.setAttribute(k, attrs[k]);
+  return el;
+}
+
+var w = $(".container").innerWidth();
+var h = $(".container").innerHeight();
+
+$("#back").css("height", h);
+
+
+const numPoints = 25;
+
+
+var points = [[0,0],[0,h],[w,0],[w,h]];
+
+var arrayX = new Uint32Array(numPoints);
+var arrayY = new Uint32Array(numPoints);
+window.crypto.getRandomValues(arrayX);
+window.crypto.getRandomValues(arrayY);
+
+for (var i = 0; i < numPoints; i++) {
+  points.push([arrayX[i] % w, arrayY[i] % h]);
+}
+
+
+var delaunay = Delaunator.from(points);
+var triangles = delaunay.triangles;
+
+function getX(i) {
+  return (points[i][0]);
+}
+function getY(i) {
+  return (points[i][1]);
+}
+
+var canvas = document.getElementById("back");
+
+$("document").ready(function () {
+  for (var i = 0; i < triangles.length; i += 3) {
+    var randNum = Math.floor(Math.random() * 20 + 220);
+    var p0 = triangles[i];
+    var p1 = triangles[i + 1];
+    var p2 = triangles[i + 2];
+    console.log(randNum);
+    var poly = makeSVG('polygon', {points: `${getX(p0)},${getY(p0)} ${getX(p1)},${getY(p1)} ${getX(p2)},${getY(p2)}`, fill: `rgb(${randNum},${randNum},${randNum})`});
+    canvas.appendChild(poly);
+  }
+});
